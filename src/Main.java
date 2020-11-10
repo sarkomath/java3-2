79419e70effa933fbb49e6855d2af1f69128b846
Nikita Novoselov <sarkomath@outlook.com>
2020-11-10 20:09:51 +1000

    add PROGRAM!

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 100;
+        int money = 1100;
+        int bonus = money / 100;
+        int total = score + bonus + money;
+        System.out.println("total is:");
+        System.out.println(total);
+    }
+}
