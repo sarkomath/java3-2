bc44ce7cbf3e4bb65235d7014fa436c66d82bdac
Nikita Novoselov <sarkomath@outlook.com>
2020-11-11 21:11:04 +1000

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        int score = 100;
-        int money = 1100;
-        int bonus = money / 100;
-        int total = score + bonus + money;
-        System.out.println("total is:");
+        long balance = 100;
+        long add = 1000;
+        long bonus = add / 100;
+        if (bonus >= 10) {
+            bonus = add / 100;
+        } else {
+            bonus = 0;
+        }
+        long total = balance + add + bonus;
+        System.out.println("Your bonus is:");
+        System.out.println(bonus);
+        System.out.println("Your total score is:");
         System.out.println(total);
     }
 }
