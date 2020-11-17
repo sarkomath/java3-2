cc1b600db1c6f9e4d59323a3122fe0efbd9caca6
Nikita Novoselov <sarkomath@outlook.com>
2020-11-17 20:56:21 +1000

    fix condition again

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         long balance = 100;
-        long add = 1000;
-        long bonus = add / 100;
-        if (bonus >= 10) {
+        long add = 600;
+        long bonus;
+        if (add / 100 >= 10) {
             bonus = add / 100;
         } else {
             bonus = 0;
